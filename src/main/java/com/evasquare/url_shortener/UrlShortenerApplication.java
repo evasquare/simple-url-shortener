b347b0c34b3b6b177a7a99bf98c69de797bc897d
evasquare <177620651+evasquare@users.noreply.github.com>
2025-01-17 14:28:17 +0900

    Delete unnecessary imports

--- src/main/java/com/evasquare/url_shortener/UrlShortenerApplication.java
+++ src/main/java/com/evasquare/url_shortener/UrlShortenerApplication.java
@@ -1,10 +1,7 @@
 package com.evasquare.url_shortener;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class UrlShortenerApplication {
